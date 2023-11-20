b3d2fc8264228a6fd1e77068d332a9a4c748d598
DESKTOP-DH25AGL\DELL <gilsrl2005@gmail.com>
2023-11-19 20:51:51 -0600

    Despensa

--- primera_parte.c
+++ primera_parte.c
@@ -1,158 +1,171 @@
 //Rama gil
-#include <stdio.h>
+#include<stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string.h> 
 #include <locale.h>
 
+// estructura: variables del producto
 typedef struct {
     char nombre[50];
-    int edad;
-    char nacionalidad[50];
-} Persona;
+    char cantidad[20];
+    float precio;
+} Producto;
 
-void agregar(Persona *personas, int *numPersonas) {
+//función para agregar productos
+void agregar(Producto *productos, int *numProductos) {
     printf("Introduce el nombre: ");
-    scanf("%s", personas[*numPersonas].nombre);
-    printf("Introduce la edad: ");
-    scanf("%d", &personas[*numPersonas].edad);
-    printf("Selecciona la nacionalidad (Mexicana, Estadounidense, Española, Chilena): ");
-    scanf("%s", personas[*numPersonas].nacionalidad);
-    
-    // ?????????????????????????? 
-    FILE *file = fopen("UNO.txt", "a");
-    fprintf(file, "%s %d %s\n", personas[*numPersonas].nombre, personas[*numPersonas].edad, personas[*numPersonas].nacionalidad);
-    fclose(file);
-    // ??????????????????????????      
-          
-    (*numPersonas)++;
-}
+    scanf("%s", productos[*numProductos].nombre);
+    printf("Introduce la cantidad con unidades: ");
+    scanf("%s", productos[*numProductos].cantidad);
+    printf("Digite el precio del producto (solo los números): ");
+    scanf("%f", &productos[*numProductos].precio);
 
-void listar(Persona *personas, int numPersonas) {
-    for(int i = 0; i < numPersonas; i++) {
-        printf("Nombre: %s, Edad: %d, Nacionalidad: %s\n", personas[i].nombre, personas[i].edad, personas[i].nacionalidad);
+    //Donde: numProductos sirve para iterar y posicionarse en el producto que se requiere
+    // Y dentro de los productos solo se pueden agregar 50
+
+
+//Apertura del Archivo, y vaciado de la información recopilada
+FILE *file = fopen("DESPENSA.txt", "a"); //El modo a para abirir o crear para escritura
+    fprintf(file, "%s %s %f\n", productos[*numProductos].nombre, productos[*numProductos].cantidad, productos[*numProductos].precio);
+    fclose(file);
+
+     (*numProductos)++;
+
+}
+// lista de los productos agregados a la DESPENSA
+void listar(Producto *productos, int numProductos) {
+    for(int i = 0; i < numProductos; i++) {
+        printf("Nombre: %s, Cantidad: %s, Precio: %f\n", productos[i].nombre, productos[i].cantidad, productos[i].precio);
     }
 }
 
-// //////////////////////////////////////////////////////////////////////////////
-void modificar(Persona *personas, int numPersonas) {
+//funcion para modificar un producto
+void modificar(Producto *productos, int numProductos) {
     char nombre[50];
-    printf("Introduce el nombre de la persona a modificar: ");
+    printf("Introduce el nombre del producto a modificar: ");
     scanf("%s", nombre);
-//    FILE *file = fopen("UNO.txt", "r");
-    FILE *nuevo = fopen("nuevoUNO.txt", "w");
+//    FILE *file = fopen("DESPENSA.txt", "r");
+    FILE *nuevo = fopen("nuevoDESPENSA.txt", "w"); // El modo w abre o crea esctritura
     //while(fscanf(file, "%s %d %s\n", personas[i].nombre, personas[i].edad, personas[i].nacionalidad)!=EOF) {
-    for(int i = 0; i < numPersonas; i++) {                     
-          if(strcmp(personas[i].nombre, nombre) == 0) { 
+    for(int i = 0; i < numProductos; i++) {   //el bucle for recorre todos los productos                   
+          if(strcmp(productos[i].nombre, nombre) == 0) { // mientras que for recorre, if esta a la espera de realizar el argumento 
+
                                                       //La función strcmp() compara dos cadenas de caracteres y devuelve un valor entero. 
-                                                      // Si las cadenas son iguales, la función devuelve el valor 0. Si las cadenas son diferentes, 
+                                                      // Si las cadenas son  iguales, la función devuelve el valor 0. Silas cadenas son diferentes, 
                                                       // la función devuelve un valor negativo si la primera cadena es menor que la segunda cadena, 
                                                       // o un valor positivo si la primera cadena es mayor que la segunda cadena.
-                                        
+                                        //En este caso necesitamos que las cadenas sean iguales
               printf("Introduce el nuevo nombre: ");
-              scanf("%s", personas[i].nombre);
-              printf("Introduce la nueva edad: ");
-              scanf("%d", &personas[i].edad);
-              printf("Selecciona la nueva nacionalidad (Mexicana, Estadounidense, Española, Chilena): ");
-              scanf("%s", personas[i].nacionalidad);
-              fprintf(nuevo, "%s %d %s\n", personas[i].nombre, personas[i].edad, personas[i].nacionalidad);
+              scanf("%s", productos[i].nombre);
+              printf("Introduce la nueva cantidad: ");
+              scanf("%s", productos[i].cantidad);
+              printf("Digite el precio del producto (solo los números): ");
+              scanf("%f", &productos[i].precio);
+              //este fprintf esta reescribiendo la nueva modificación en el lugar del producto cambiado
+              fprintf(nuevo, "%s %s %f\n", productos[i].nombre, productos[i].cantidad, productos[i].precio);
 //              (*numPersonas)++;
               //return;
          }
+         /*en el caso de que la cadena no sea la misma se vuelven a vaciar los mismos datos de los prodcutos 
+         en el nuevo archivos, sin hacer cambios*/
          else
-             fprintf(nuevo, "%s %d %s\n", personas[i].nombre, personas[i].edad, personas[i].nacionalidad);
+             fprintf(nuevo, "%s %s %f\n", productos[i].nombre, productos[i].cantidad, productos[i].precio);
 //             (*numPersonas)++;
     }
-//    fclose(file);
+//    fclose(file);//se cierra el archivo
     fclose(nuevo);
-    remove("UNO.txt");
-    rename("nuevoUNO.txt","UNO.txt");
+    remove("DESPENSA.txt"); //Se borra el archivo anterior
+    rename("nuevoDESPENSA.txt","DESPENSA.txt"); // al archivo nuevo se le vuelve a asignar el nombre del anterior 
 }
-// //////////////////////////////////////////////////////////////////////////////
 
-
-void borrar(Persona *personas, int *numPersonas) {
+//funcion para borrar productos
+void borrar(Producto *productos, int *numProductos) { //tengo 4 productos
     char nombre[50];
-    printf("Introduce el nombre de la persona a borrar: ");
-    scanf("%s", nombre);
-    for(int i = 0; i < *numPersonas; i++) {
-        if(strcmp(personas[i].nombre, nombre) == 0) {
-            for(int j = i; j < *numPersonas - 1; j++) {
-                personas[j] = personas[j + 1];
+    printf("Introduce el nombre del produto a borrar: "); 
+    scanf("%s", nombre); //Manzana
+    for(int i = 0; i < *numProductos; i++) { //el iterador da 4 vueltas 0,1,2,3
+        if(strcmp(productos[i].nombre, nombre) == 0) { // si las cadenas son idénticas(Ejemplo en i=2 Manzana) hacer{}
+            for(int j = i; j < *numProductos - 1; j++) { //j=2     numProductos=4-1=3 j va una vuelta en j=i=2
+                productos[j] = productos[j + 1];  /*primer vuelta producto 2(manzana)= produto3(Platano)
+                por lo que, ese producto será reemplazado por el prodcuto 3 que es platano, ahora
+                 platano es el producto 2*/
             }
-            (*numPersonas)--;
-            // ??????????????????????????      
-            FILE *file = fopen("nuevoUNO.txt", "w"); 
-            for(int i = 0; i < *numPersonas; i++) {
-                fprintf(file, "%s %d %s\n", personas[i].nombre, personas[i].edad, personas[i].nacionalidad);
+            (*numProductos)--; //entonces se reducira en 1 el numero de productos
+               
+            FILE *file = fopen("nuevoDESPENSA.txt", "w"); 
+            for(int i = 0; i < *numProductos; i++) {
+                fprintf(file, "%s %s %f\n", productos[i].nombre, productos[i].cantidad, productos[i].precio);
             }
-            fclose(file);
-            remove("UNO.txt");
-            rename("nuevoUNO.txt","UNO.txt");  
-            // ??????????????????????????        
+            fclose(file); // se cierra el archivo
+            remove("DESPENSA.txt");
+            rename("nuevoDESPENSA.txt","DESPENSA.txt"); // al archivo nuevo se le vuelve a asignar el nombre del anterior
+               
             return;
         }
     }
-    printf("No se encontró a la persona.\n");
+    printf("No se encontró el producto.\n");
 }
-
-void buscar(Persona *personas, int numPersonas) {
+//Función para buscar algun producto
+void buscar(Producto *productos, int numProductos) {
     char nombre[50];
     printf("Introduce el nombre de la persona a buscar: ");
     scanf("%s", nombre);
-    for(int i = 0; i < numPersonas; i++) {
-        if(strcmp(personas[i].nombre, nombre) == 0) {
-            printf("Nombre: %s, Edad: %d, Nacionalidad: %s\n", personas[i].nombre, personas[i].edad, personas[i].nacionalidad);
+    for(int i = 0; i < numProductos; i++) {
+        if(strcmp(productos[i].nombre, nombre) == 0) {
+            printf("Nombre: %s, Cantidad: %s, Precio: %f\n", productos[i].nombre, productos[i].cantidad, productos[i].precio);
             return;
         }
     }
     printf("No se encontró a la persona.\n");
 }
-
-void guardar(Persona *personas, int numPersonas) {
-    // FILE *file = fopen("UNO.txt", "w");
-    FILE *file = fopen("UNO.txt", "a");
+//Función para guardar
+void guardar(Producto *productos, int numProductos) {
+    // FILE *file = fopen("DESPENSA.txt", "w");
+    FILE *file = fopen("DESPENSA.txt", "a");/* se abre el archivo con modo 'a'para Abrir o 
+    crea para escritura. Agrega después de cualquier contenido previo.*/
     if(file == NULL) {
         printf("No se pudo abrir el archivo.\n");
         return;
     }
-    for(int i = 0; i < numPersonas; i++) {
-        fprintf(file, "%s %d %s\n", personas[i].nombre, personas[i].edad, personas[i].nacionalidad);
+    for(int i = 0; i < numProductos; i++) {
+        fprintf(file, "%s %s %f\n", productos[i].nombre, productos[i].cantidad, productos[i].precio);
     }
     
     fclose(file);
 }
-
-void cargar(Persona *personas, int *numPersonas) {
-    FILE *file = fopen("UNO.txt", "r");
+//Función para cargar
+void cargar(Producto *productos, int *numProductos) {
+    FILE *file = fopen("DESPENSA.txt", "r"); //Se abre la lectura del archivo con el modo 'r'
     if(file == NULL) {
         printf("No se pudo abrir el archivo.\n");
         return;
     }
-    while(fscanf(file, "%s %d %s", personas[*numPersonas].nombre, &personas[*numPersonas].edad, personas[*numPersonas].nacionalidad) == 3) {
-        (*numPersonas)++;
+    while(fscanf(file, "%s %s %f", productos[*numProductos].nombre, productos[*numProductos].cantidad, &productos[*numProductos].precio) == 3) { 
+        //si fscanf lee los 3 campos: nombre, cantidad y precio numProductos incrementa de 1 en 1 
+        (*numProductos)++;
     }
     fclose(file);
 }
-
+//función principal int main
 int main() {
     
     setlocale(LC_ALL,"");
     
-    Persona personas[50];
-    int numPersonas = 0;
+    Producto productos[50];//cantidad posible de productos en la Despensa
+    int numProductos = 0;
     int opcion;
-    // ?????????????????????????????????
-    cargar(personas, &numPersonas);
-    // ?????????????????????????????????
+    
+    cargar(productos, &numProductos);
+    
     
     do {
-        printf("1. Agregar persona\n");
-        printf("2. Listar personas\n");
-        printf("3. Modificar persona\n");
-        printf("4. Borrar persona\n");
-        printf("5. Buscar persona\n");
-//        printf("6. Guardar personas\n");
-//        printf("7. Cargar personas\n");
+        printf("1. Agregar producto\n");
+        printf("2. Listar productos\n");
+        printf("3. Modificar producto\n");
+        printf("4. Borrar producto\n");
+        printf("5. Buscar producto\n");
+//        printf("6. Guardar personas\n"); //realizado por las funciones
+//        printf("7. Cargar personas\n");   //realizado por las funciones
         printf("6. Salir\n");
         printf("Selecciona una opción: ");
         scanf("%d", &opcion);
@@ -160,29 +173,29 @@ int main() {
         switch(opcion) {
             case 1:
                 system("cls");
-                agregar(personas, &numPersonas);
+                agregar(productos, &numProductos);
                 break;
             case 2:
                 system("cls");
-                listar(personas, numPersonas);
+                listar(productos, numProductos);
                 break;
             case 3:
                 system("cls");
-                modificar(personas, numPersonas);
+                modificar(productos, numProductos);
                 break;
             case 4:
                 system("cls");
-                borrar(personas, &numPersonas);
+                borrar(productos, &numProductos);
                 break;
             case 5:
                 system("cls");
-                buscar(personas, numPersonas);
+                buscar(productos, numProductos);
                 break;
 //            case 6:
-                guardar(personas, numPersonas);
+                guardar(productos, numProductos); //el usaurio saldra y se guardaran sus productos en el archivo
                 break;
 //            case 7:
-//               cargar(personas, &numPersonas);
+//               cargar(productos, &numProductos);
 //                break;
         }
     } while(opcion != 6);
